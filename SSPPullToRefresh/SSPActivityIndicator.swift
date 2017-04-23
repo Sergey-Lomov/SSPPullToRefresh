d2e740334a60f1f0532add57ce1ea35f057b92ba
Sergey Lomov <sergeylomov@Rostyslav.local>
2017-04-24 00:07:37 +0300

    Project init

---
+++ SSPPullToRefresh/SSPActivityIndicator.swift
@@ -0,0 +1,16 @@
+//
+//  SSPActivityIndicator.swift
+//  BezierAnimation
+//
+//  Created by Sergey Lomov on 4/18/17.
+//  Copyright © 2017 Rozdoum. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SSPActivityIndicator:UIView {
+    func reset() {}
+    func stopAnimating() {}
+    func startAnimating() {}
+}
---
+++ SSPPullToRefresh/SSPPullToRefreshView.swift
@@ -0,0 +1,26 @@
+//
+//  SSPPullToRefreshView.swift
+//  BezierAnimation
+//
+//  Created by Sergey Lomov on 4/19/17.
+//  Copyright © 2017 Rozdoum. All rights reserved.
+//
+
+import UIKit
+
+protocol SSPPullToRefreshDelegate {
+    func preparationDidFinish(refreshView:SSPPullToRefreshView)
+    func compressingDidFinish(refreshView:SSPPullToRefreshView)
+   // func pullReleaseDidFinish(refreshView:SSPPullToRefreshView)
+}
+
+class SSPPullToRefreshView : UIView {
+    var delegate:SSPPullToRefreshDelegate?
+    
+    func setPullProgress(progress:CGFloat) {}
+    func releasePull() {}
+    func startUpdateAnimation() {}
+    func startCompressingAnimation() {}
+    
+    var activityIndicatorView:SSPActivityIndicator?
+}
