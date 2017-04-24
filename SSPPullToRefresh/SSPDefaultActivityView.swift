2f3808ad6badb769bac7d43b9cf8d7543a7cb7d1
Sergey Lomov <sergeylomov@Rostyslav.local>
2017-04-24 20:10:43 +0300

    Added SSPLayerSpeedActivityView with activity methods implementations by layer speed. Added wrapper for standard UIActivityIndicatorView

---
+++ SSPPullToRefresh/SSPDefaultActivityView.swift
@@ -0,0 +1,43 @@
+//
+//  SSPDefaultActivityView.swift
+//  BezierAnimation
+//
+//  Created by Sergey Lomov on 4/24/17.
+//  Copyright © 2017 Rozdoum. All rights reserved.
+//
+
+import UIKit
+
+class SSPDefaultActivityView: SSPLayerSpeedActivityView {
+
+    var activity:UIActivityIndicatorView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addDefaultActivity()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        addDefaultActivity()
+    }
+    
+    private func addDefaultActivity () {
+        activity = UIActivityIndicatorView()
+        activity.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(activity)
+        activity.isHidden = false
+        activity.startAnimating()
+        animationAddingTime = CACurrentMediaTime()
+        
+        let leftConstraint = NSLayoutConstraint(item: activity, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.left, multiplier: 1, constant: 0)
+        let rightConstraint = NSLayoutConstraint(item: activity, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.right, multiplier: 1, constant: 0)
+        let topConstraint = NSLayoutConstraint(item: activity, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.top, multiplier: 1, constant: 0)
+        let bottomtConstraint = NSLayoutConstraint(item: activity, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: 0)
+        
+        NSLayoutConstraint.activate([leftConstraint, rightConstraint, topConstraint, bottomtConstraint])
+    }
+    
+    
+}
---
+++ SSPPullToRefresh/SSPLayerSpeedActivityView.swift
@@ -0,0 +1,36 @@
+//
+//  SSPLayerSpeedActivityView.swift
+//  BezierAnimation
+//
+//  Created by Sergey Lomov on 4/24/17.
+//  Copyright © 2017 Rozdoum. All rights reserved.
+//
+
+import UIKit
+import SSPPullToRefresh
+
+class SSPLayerSpeedActivityView: SSPActivityIndicatorView {
+
+    // Should be setted in subclass at animation adding
+    internal var animationAddingTime:CFTimeInterval?
+    
+    open override func reset() {
+        layer.speed = 0.0
+        layer.timeOffset = animationAddingTime!
+    }
+    
+    open override func startAnimating() {
+        let stopedLayerTime = layer.convertTime(CACurrentMediaTime(), from: nil)
+        layer.speed = 1.0
+        layer.timeOffset = 0.0
+        let runedLayerTime = layer.convertTime(CACurrentMediaTime(), from: nil)
+        layer.timeOffset = -1 * (runedLayerTime - stopedLayerTime)
+    }
+    
+    open override func stopAnimating() {
+        let layerTime = layer.convertTime(CACurrentMediaTime(), from: nil)
+        layer.speed = 0
+        layer.timeOffset = layerTime
+    }
+
+}
